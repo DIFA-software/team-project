c61bd220f6634e84ceccf77d19cce7c9598613e6
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2021-11-05 09:30:48 +0200

    React File Opened

---
+++ ecommerce/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+       <div>
+        <h1> This is eCommerce by DIFA Software </h1>
+       </div>
+    </div>
+  );
+}
+
+export default App;
